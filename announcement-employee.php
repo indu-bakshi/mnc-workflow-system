9102db0dca5d6e9ec86cac59249f392a7afee193
Indu Bakshi <bakshi.webworld@gmail.com>
2020-12-14 12:15:43 +0530

    Update announcement-employee.php

--- announcement-employee.php
+++ announcement-employee.php
@@ -69,6 +69,7 @@ $result_ann = mysqli_query($conn,"SELECT * from announcement ORDER BY date DESC;
     <div class="d-flex text-muted pt-3">
       <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
         <?php $curr_date=$row["date"];
+          date_default_timezone_set("India/Mumbai");
         $curr_date = date( "d F, Y, h:i a", strtotime($curr_date));
         ?>
       <p class="pb-3 mb-0 small lh-sm border-bottom" style="padding-left:20px">
